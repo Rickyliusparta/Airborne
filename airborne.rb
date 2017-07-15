e27dd283e5c96022608ea27194714d8c3b7ad6f6
Ricky Liu <rliu@spartaglobal.co>
2017-07-15 20:08:16 +0100

    changes

--- airborne.rb
+++ airborne.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "postcode works" do
 
 # it "should report to a single postcode" do
-# 		x = get '/sm69rf'
+# 		x = get '/sm68rf'
 # 		y = JSON.parse(x)
 # 		y.each do |key,value|
 # 			puts key,value	
@@ -18,40 +18,40 @@ describe "postcode works" do
 	end
 
 	it 'should return the quality vaule' do
-		x = get '/sm69rf'
+		x = get '/sm68rf'
 		y = JSON.parse(x)
 		expect(y['result']['quality']).to be 1
 	end
 
 	it 'should return the correct easting vaule' do
-		get '/sm69rf'
+		get '/sm68rf'
 		expect_json(result: {eastings: 529536}) 
 		end
 
 	it 'should return the correct northings value' do
-		get '/sm69rf'
+		get '/sm68rf'
 		expect_json(result: {northings: 163297})
 	end
 
 	it 'should return admin district with values' do
-		x = get '/sm69rf'
+		x = get '/sm68rf'
 		y = JSON.parse(x)
 		expect(y['result']['codes']['admin_district']).to eq 'E09000029'
 	end
 
 	it 'should return admin district with values' do
-		get '/sm69rf'
+		get '/sm68rf'
 		expect_json(result: {codes: {admin_district: 'E09000029'}})
 	end
 
 	it 'should return something' do
-		get '/sm69rf'
+		get '/sm68rf'
 		expect_json(:result {:country => England})
 	end
 	
 	it 'should get all postcode' do
-		get '/sm69rf'
-		expect_json('result', {:postcode => 'SM6 9RF', :quality => 1, :eastings => 529536, :northings => 163297, :country => 'England', :nhs_ha => 'London', :longitude => -0.141137589483951, :latitude => 51.3539906003041, :parliamentary_constituency => 'Carshalton and Wallington', :european_electoral_region => 'London', :primary_care_trust => 'Sutton and Merton', :region => 'London', :lsoa => 'Sutton 017A', :msoa => 'Sutton 017', :incode => '9RF', :outcode => 'SM6', :admin_district => 'Sutton', :parish => 'Sutton, unparished area', :admin_county => nil, :ccg => 'NHS Sutton', :nuts => 'Merton, Kingston upon Thames and Sutton', ':codes', :admin_district => 'E09000029'}})
+		get '/sm68rf'
+		expect_json('result', {:postcode => 'SM6 8RF', :quality => 1, :eastings => 529536, :northings => 163297, :country => 'England', :nhs_ha => 'London', :longitude => -0.141137589483951, :latitude => 51.3539906003041, :parliamentary_constituency => 'Carshalton and Wallington', :european_electoral_region => 'London', :primary_care_trust => 'Sutton and Merton', :region => 'London', :lsoa => 'Sutton 017A', :msoa => 'Sutton 017', :incode => '9RF', :outcode => 'SM6', :admin_district => 'Sutton', :parish => 'Sutton, unparished area', :admin_county => nil, :ccg => 'NHS Sutton', :nuts => 'Merton, Kingston upon Thames and Sutton', ':codes', :admin_district => 'E09000029'}})
 	end
 end
 
